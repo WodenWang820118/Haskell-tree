45e3e128b2b00e9470822f49372cbf5eac9b6145
WodenWang820118 <alex0972112871@gmail.com>
2021-06-11 15:08:29 +0000

    tree delete node

--- tree.hs
+++ tree.hs
@@ -3,23 +3,23 @@
 -- deriving Show to show Node in GHCI
 data Tree a = Nil | Node a (Tree a) (Tree a) deriving Show
 
--- | treeInsert takes any number and
+-- | tree_insert takes any number and
 --   and return a Node
-treeInsert :: Ord a => a -> Tree a -> Tree a
-treeInsert x Nil = Node x Nil Nil -- build a Node
-treeInsert x (Node root left right)
+tree_insert :: Ord a => a -> Tree a -> Tree a
+tree_insert x Nil = Node x Nil Nil -- build a Node
+tree_insert x (Node root left right)
   | x == root = Node root left right -- avoid duplicate
-  | x < root = Node root (treeInsert x left) right -- insert a node on a left
-  | x > root = Node root left (treeInsert x right) -- insert a node on the right
+  | x < root = Node root (tree_insert x left) right -- insert a node on a left
+  | x > root = Node root left (tree_insert x right) -- insert a node on the right
 
--- | treeListInsert takes a list of numbers
---   and return a tree using treeInsert function
-treeListInsert :: (Ord a) => [a] -> Tree a
+-- | tree_list_insert takes a list of numbers
+--   and return a tree using tree_insert function
+tree_list_insert :: (Ord a) => [a] -> Tree a
 -- use the lambda function here; acc accumulates the tree
 -- each time insert a node from the leftmost list 
 -- for example [7,12,6,4,8] would start from 7
 -- foldl function Nil List
-treeListInsert xs = foldl (\acc a -> treeInsert a acc) Nil xs
+tree_list_insert xs = foldl (\acc a -> tree_insert a acc) Nil xs
 
 ------------------------ Traversal ------------------------
 
@@ -56,19 +56,62 @@ is_balance (Node root left right)
 ------------------------ Tree average ------------------------
 -- | get the double value from a tree
 --   return the average
-treeAverage :: Tree Double -> Double
-treeAverage Nil = 0.0 -- empty tree
-treeAverage tree =
-  let (sum1,count1) = treeAverage' tree
+tree_average :: Tree Double -> Double
+tree_average Nil = 0.0 -- empty tree
+tree_average tree =
+  let (sum1,count1) = tree_average' tree
   in sum1/count1
 
 -- | helper function for calculating the average
 --   which take a tree and return its node summation and counts
-treeAverage' :: Tree Double -> (Double,Double)
-treeAverage' Nil = (0.0,0.0)
-treeAverage' (Node root left right) =
-  let (sumL,countL) = treeAverage' left
-      (sumR,countR) = treeAverage' right
+tree_average' :: Tree Double -> (Double,Double)
+tree_average' Nil = (0.0,0.0)
+tree_average' (Node root left right) =
+  let (sumL,countL) = tree_average' left
+      (sumR,countR) = tree_average' right
   in ((root+sumL+sumR),(1+countL+countR))
 
 ------------------------ Tree average ends ------------------------
+
+------------------------ Tree delete node ------------------------
+
+-- | delete_node takes a key and a tree
+--   the function finds the key matches the node
+--   and pass the key and tree to the helper function delete_node'
+delete_node :: Ord a => a -> Tree a -> Tree a
+delete_node _ Nil = Nil
+delete_node x tree@(Node root left right)
+  | x < root = Node root (delete_node x left) right
+  | x > root = Node root left (delete_node x right)
+  | x == root = delete_node' tree
+
+
+-- for example
+--     7
+--    / \
+--   6  Nil
+--  / \
+-- Nil Nil
+--
+-- delete the node 7 would be
+--     6
+--    / \
+--   Nil Nil
+-- which bring the left subtree as new root
+--
+-- | delete_node' takes a tree
+--   the key already matched the root value
+--   in thie function, to delete a node
+--   bring the left subtree or right subtree as root if the other subtree is Nil
+--   otherwise, use right subtree, retrieve its left_most_elem as a newRoot
+delete_node' :: Ord a => Tree a -> Tree a
+delete_node' (Node root Nil right) = right -- bring the right subtree as new root
+delete_node' (Node root left Nil) = left -- bring the left subtree as new root
+delete_node' (Node root left right) = (Node newRoot left right) where
+  newRoot = left_most_elem right -- or the right most elemnet of the left subtree
+
+left_most_elem :: Ord a => Tree a -> a
+left_most_elem (Node root Nil _) = root -- there's no more left subtree, get its root value
+left_most_elem (Node _ left _) = left_most_elem left -- otherwise, keep search its left subtree
+
+------------------------ Tree delete node ends ------------------------
